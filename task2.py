7ae2c7275617875c0b4bbbd6dc7c823d7f5f56a1
D-Gr3at <billeibinabo@gmail.com>
2022-05-26 13:15:43 +0100

    added task2

---
+++ task2.py
@@ -0,0 +1,8 @@
+def my_sort(number):
+    return abs(number - 50)
+
+
+if __name__ == '__main__':
+    my_list = [60, 0, 30, 100, 35, 80, 77, 19, 27, 90]
+    my_list.sort(key=my_sort)
+    print(my_list)
